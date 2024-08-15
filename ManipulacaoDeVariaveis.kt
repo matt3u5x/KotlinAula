329e9d57a2543433ad69229d8dfae8236a88eadc
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-08-14 21:48:07 -0300

    Create ManipulacaoDeVariaveis.kt

---
+++ ManipulacaoDeVariaveis.kt
@@ -0,0 +1,37 @@
+fun main (args : Array<String>){
+
+    var nome: String = "Mateus"
+    var idade: Int = 19
+    var altura: Double = 1.70
+    var estudante: Boolean = true
+
+    var nome2: String = "Luiz"
+    var idade2:Int = 19
+    var altura2:Double = 1.90
+    var estudante2: Boolean = true
+
+    var somaIdade = idade + idade2
+    var somaAltura = altura*2
+    var somaAltura2 = altura*2
+
+
+    println("Olá $nome! Aqui estão seus dados:")
+    println("Olá $nome")
+    println("Idade: $idade")
+    println("Altura: $altura")
+    println("Soma de idade: $somaIdade")
+    println("Soma de altura: $somaAltura")
+    println("É estudante")
+
+
+
+    println("Olá $nome2 Aqui está os seus dados:")
+    println("Nome $nome2")
+    println("Altura $altura2")
+    println("Soma de idade: $somaIdade")
+    println("Soma de altura: $somaAltura2")
+    println("É estudante")
+
+
+    println("Thanks for using my sistem")
+}
