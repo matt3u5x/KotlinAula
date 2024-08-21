6c3fbf20e4b0adbe3a0cc19741a41856c88fd467
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-08-20 21:22:15 -0300

    Update DesafioManipulaçao.kt

--- DesafioManipulaçao.kt
+++ DesafioManipulaçao.kt
@@ -1,4 +1,7 @@
+import java.text.DecimalFormat
+
 fun main(args: Array<String>) {
+
 
     var nomeProduto: String = "Teclado"
     var precoUnitario: Int = 350
@@ -6,12 +9,24 @@ fun main(args: Array<String>) {
     var imposto: Double = 25.0
     var margemLucroDesejada: Double = 75.0
 
+    val df = DecimalFormat("#,###.00")
+
+    println()
+
     var valorTotalSemImposto = precoUnitario * quantidadeCompra
     var valorTocalComImposto = valorTotalSemImposto + (valorTotalSemImposto * (imposto / 100))
 
     val precoSugeridoLucro = precoUnitario + (precoUnitario * (margemLucroDesejada / 100))
 
+    println("Produto: $nomeProduto")
+    println("Preço unitário $precoUnitario")
+    println("Quantidade de Compra: $quantidadeCompra")
+    println("Impostos: $imposto %")
+    println("Lucro desejado: $margemLucroDesejada %")
+    println("O valor do produdo sem imposto será de: $valorTotalSemImposto e o valor com impostos será de: $valorTocalComImposto")
+    println("Preço final: ${df.format(precoSugeridoLucro)}")
 
-    
+
+
 
 }
